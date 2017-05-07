475278b5d83765cd2a322cbe497c55ca1ed9fcbb
Szymon <szymon.gwozdz@outlook.com>
2017-05-07 19:55:46 +0200

    Add CampModel.cs

---
+++ MyCodeCamp/Models/CampModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyCodeCamp.Models
+{
+    public class CampModel
+    {
+        public int Id { get; set; }
+        public string Moniker { get; set; }
+        public string Name { get; set; }
+        public DateTime EventDate { get; set; } = DateTime.MinValue;
+        public int Length { get; set; }
+        public string Description { get; set; }
+
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Address3 { get; set; }
+        public string CityTown { get; set; }
+        public string StateProvince { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+}
